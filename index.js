717aca54b10e34f522e4ed9bbc764cc91364dd28
Caio Silva <caiovosilva@outlook.com>
2019-08-15 13:38:31 -0300

    post rule

--- index.js
+++ index.js
@@ -5,18 +5,39 @@ const app = express();
 app.use(express.json());
 
 const rules = [
-    {specificDay: [
+    {
+        specificDay: [
             {
-            day: "25-01-2018",
-            intervals: [{ start: "14:30", end: "15:00" }, { start: "15:10", end: "15:30" }]
+                day: "25-01-2018",
+                intervals: [{ start: "14:30", end: "15:00" }, { start: "15:10", end: "15:30" }]
             },
             {
-            day: "26-01-2018",
-            intervals: [{ start: "14:30", end: "15:00" }, { start: "15:00", end: "15:30" }]
+                day: "26-01-2018",
+                intervals: [{ start: "14:30", end: "15:00" }, { start: "15:00", end: "15:30" }]
+            }
+        ]
+    },
+    {
+        daily: [
+            {
+                intervals: [{ start: "9:30", end: "10:00" }, { start: "19:10", end: "20:30" }]
             },
             {
-            day: "29-01-2018",
-            intervals: [{ start: "10:40", end: "11:00" }, { start: "15:00", end: "15:30" }]
+                intervals: [{ start: "4:30", end: "5:00" }, { start: "5:20", end: "5:30" }]
+            }
+        ]
+    },
+    {
+        weekly: [
+            {
+                day: 0,
+                start: "9:30", 
+                end: "10:00"
+            },
+            {
+                day: 3,
+                start: "9:30", 
+                end: "10:00"
             }
         ]
     }
@@ -42,8 +63,8 @@ app.get('/', (req, res) => {
     res.send(rules)
 })
 
-app.get('/api/courses', (req,res) => {
-    res.send(courses)
+app.get('/api/rules', (req,res) => {
+    res.send(rules)
 })
 
 app.get('/api/courses/:id', (req, res) => {
@@ -54,19 +75,58 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course)
 })
 
-app.post('/api/rules', (req,res) => {
-    let rule = req.body
-    if(!rule.specificDay && !rule.daily && !rule.wekly)
-        return res.status(400).send(`choose if it's a rule for a specific day, daily or weekly`)
+function postRule(rule, res) {     
     const moment = require('moment')
-    let newRule
+    const newRule = {}
 
     if(rule.specificDay) {
-            if('day' in rule.specificDay)
-                let day = moment(rule.specificDay.day, 'DD-MM-YYYY')
-            if('intervals' in rule.specificDay && Array.isArray(intervals))
-                fazer um for aqui 
+        newRule.type = 'specificDay'
+        newRule.intervals = []
+        if('day' in rule.specificDay) {
+            const date = moment(rule.specificDay.day, 'DD-MM-YYYY')
+            if(date.isValid()) newRule.day = date._i
         }
+        if('intervals' in rule.specificDay && Array.isArray(rule.specificDay.intervals))
+            for(let k in rule.specificDay.intervals) {
+                const element = rule.specificDay.intervals[k]
+                const newInterval = {}
+                if('start' in element) newInterval.start = element.start
+                if('end' in element) newInterval.end = element.end
+                if(newInterval.start && newInterval.end) newRule.intervals.push(newInterval)
+            }
+        if(!newRule.day || newRule.intervals.length<1)
+            return res.status(400).send(`Bad request - visit the home screen for the API reference`)
+    }
+    else 
+    if(rule.daily) {
+        newRule.type = 'daily'
+        newRule.intervals = []
+        if('intervals' in rule.daily && Array.isArray(rule.daily.intervals))
+            for(let k in rule.daily.intervals) {
+                const element = rule.daily.intervals[k]
+                const newInterval = {}
+                if('start' in element) newInterval.start = element.start
+                if('end' in element) newInterval.end = element.end
+                if(newInterval.start && newInterval.end) newRule.intervals.push(newInterval)
+            }
+        if(newRule.intervals.length<1)
+            return res.status(400).send(`Bad request - visit the home screen for the API reference`)
+    }
+    else 
+    if(rule.weekly) {
+        const element = rule.weekly
+        newRule.type = 'weekly'
+        if('day' in element) newRule.day = parseInt(element.day)
+        if('start' in element) newRule.start = element.start
+        if('end' in element) newRule.end = element.end
+    }
+    else
+        return res.status(400).send(`Rule for a specific day, daily or weekly not found`)
+    return newRule
+}
+
+app.post('/api/rules', (req,res) => {
+    res.send(postRule(req.body, res))
     //const result = validateRule(req.body)
     // if(result.error)
     //     return res.status(400).send(result.error.details[0].message)
@@ -114,6 +174,6 @@ app.delete('/api/courses', (req,res) => {
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
-var moment = require('moment');
-var date = moment('20-04-2012', 'DD-MM-YYYY');
-console.log(date)
+// var moment = require('moment');
+// var date = moment('20-04-2012', 'DD-MM-YYYY');
+// console.log(date)
