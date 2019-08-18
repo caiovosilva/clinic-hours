d91852e5e3ed345f2cfd20972e567468072aa53f
Caio Silva <caiovosilva@outlook.com>
2019-08-17 23:59:50 -0300

    No more duplicated days with different intervals when /api/workinghours

--- businessLogic.js
+++ businessLogic.js
@@ -2,53 +2,45 @@ const moment = require('moment')
 
 function getWorkingHours(startDate, endDate, rules, callBack) {
     const response = []
-    const specificDayRules = rules.filter(x =>
-        x.type === `specificDay` &&
-        moment(x.day, 'DD-MM-YYYY').isSameOrAfter(startDate , 'day') &&
-        endDate.isSameOrAfter(moment(x.day, 'DD-MM-YYYY'), 'day'))
-    if(specificDayRules.length > 0) 
-        formatSpecificDaysResponse(specificDayRules, response)
-
-    const dailyRules = rules.filter(x =>
-        x.type === `daily`)
-    const weeklyRules= rules.filter(x =>
-        x.type === `weekly`)
-    if(dailyRules.length > 0 || weeklyRules.length > 0 ) 
-        formatDailyAndWeeklyResponse(dailyRules, weeklyRules, startDate, endDate, response)
-
-    callBack(response)
-}
-
-function formatSpecificDaysResponse (rules, response) { 
-    rules.forEach(element => {
-        const newRule = {}
-        newRule.day = element.day
-        newRule.intervals = element.intervals
-        response.push(newRule)
-    })
-}
-
-//I was going to put daily and weekly formats in separate functions, but to only iterate through the days only once, I put then together
-function formatDailyAndWeeklyResponse (dailyRules, weeklyRules, startDate, endDate, response) { 
     let day = startDate.clone()
-    while(endDate.isSameOrAfter(day)) {
-        dailyRules.forEach(element => {
-            const newRule = {}
-            newRule.day = day.format('DD-MM-YYYY').toString()
-            newRule.intervals = element.intervals
-            response.push(newRule)
-        })
-        const dayOfWeek = day.day()
-        weeklyRules.forEach(element => {
-            if(dayOfWeek === element.dayOfWeek){
-                const newRule = {}
-                newRule.day = day.format('DD-MM-YYYY').toString()
-                newRule.intervals = element.intervals
-                response.push(newRule)
+    while(endDate.isSameOrAfter(startDate)) {
+        const newRule = {}
+        newRule.day = startDate.format('DD-MM-YYYY').toString()
+        newRule.intervals = []
+        rules.forEach(element => {
+            switch (element.type) {
+                case `specificDay`:
+                    if(moment(element.day, 'DD-MM-YYYY').isSame(startDate))
+                        element.intervals.forEach(interval => {
+                            newRule.intervals.push(interval)
+                        });
+                    break;
+                case `weekly`:
+                    const dayOfWeek = startDate.day()
+                    if(dayOfWeek === element.dayOfWeek)
+                        element.intervals.forEach(interval => {
+                            newRule.intervals.push(interval)
+                        });
+                    break;
+                case `daily`:
+                    element.intervals.forEach(interval => {
+                        newRule.intervals.push(interval)
+                    });
+                    break;
             }
         })
-        day.add(1, 'days')
+        if(newRule.intervals.length > 0)
+            response.push(newRule)
+        startDate.add(1, 'd')
     }
+
+    response.sort((a, b) => {
+        if(moment(a).isAfter(moment(b)))
+            return 1
+        return -1
+    })
+
+    callBack(response)
 }
 
 module.exports.getWorkingHours = getWorkingHours
